f846726e997298f6018dabc9eab0f97cdfbf62d3
boskojaksic <boskojaksic2407995@gmail.com>
2025-10-06 10:17:11 +0200

    env files security

--- config/env.ts
+++ config/env.ts
@@ -1,30 +1,18 @@
-
 export enum Environment {
     DEVELOPMENT = 'development',
     PRODUCTION = 'production',
 }
 
-// Default to development environment
 const ENV = process.env.NODE_ENV || Environment.DEVELOPMENT;
 
 interface Config {
     weatherApiKey: string;
     environment: Environment;
-
 }
 
-const developmentConfig: Config = {
-    weatherApiKey:'cc67fa63053633250f6e2801464593c3',
-    environment: Environment.DEVELOPMENT,
+const config: Config = {
+    weatherApiKey: process.env.EXPO_PUBLIC_WEATHER_API_KEY!,
+    environment: ENV === 'production' ? Environment.PRODUCTION : Environment.DEVELOPMENT,
 };
-
-const productionConfig: Config = {
-    weatherApiKey:'cc67fa63053633250f6e2801464593c3',
-
-    environment: Environment.PRODUCTION
-};
-
-
-const config: Config = ENV === Environment.PRODUCTION ? productionConfig : developmentConfig;
 
 export default config;
